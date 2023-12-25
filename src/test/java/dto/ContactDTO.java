fc449927b747d3ef849f122fdb54cb9033255280
Oleg Sher <olegsher@revuze.it>
2023-12-25 21:00:29 +0200

    mobile test 1

--- src/test/java/dto/ContactDTO.java
+++ src/test/java/dto/ContactDTO.java
@@ -1,2 +1,20 @@
-package dto;public class ContactDTO {
+package dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@Builder
+@ToString
+
+public class ContactDTO {
+    String name;
+    String lastName;
+    String phone;
+    String email;
+    String description;
+    String address;
 }
--- src/test/java/dto/UserDTO.java
+++ src/test/java/dto/UserDTO.java
@@ -1,2 +1,16 @@
-package dto;public class UserDTO {
+package dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@Builder
+@ToString
+
+public class UserDTO {
+    String email;
+    String password;
 }
